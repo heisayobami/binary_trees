1e66152e701d2d4f9fb195373addfe79429b2f71
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-03-08 23:59:51 +0100

    Root node detector

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -10,5 +10,9 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	return ((!node || node->parent) ? 0 : 1);
+    if (node==NULL)
+        return 0;
+    if (node->parent==NULL)
+        return 1;
+    return 0;
 }
