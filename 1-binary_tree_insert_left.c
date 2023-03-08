dc74aa96f4e1310d9b6c9c4144e7ee5a634bf012
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-03-08 20:08:14 +0100

    Insert @ left node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,6 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		temp = parent->left;
 		newnode->left = temp;
+		temp->parent = newnode;
 	}
 	else
 		newnode->left = NULL;
