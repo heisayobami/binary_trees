a5807b1e1c61974145ab141ce062d8ef8383edae
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-03-08 23:54:00 +0100

    Leaf node detector

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,5 +10,9 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	return ((!node || node->left || node->right) ? 0 : 1);
+    if (node==NULL)
+        return 0;
+    if (node->left==NULL && node->right==NULL)
+        return 1;
+    return 0;
 }
