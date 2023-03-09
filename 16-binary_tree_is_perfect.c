ffc029b968835c686265435d77bc4af05c2c7bf2
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-03-09 22:25:58 +0100

    Perfect BT test

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -42,3 +42,28 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
     
     return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: tree to measure the height of
+ *
+ * Return: height of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t height_left = 0, height_right = 0;
+	
+    if (tree==NULL)
+        return 0;
+	
+    if (tree->left)
+        height_left = 1 + binary_tree_height(tree->left);
+    if (tree->right)
+        height_right = 1 + binary_tree_height(tree->right);
+	
+    if (height_left > height_right)
+        return height_left;
+    else
+        return height_right;
+}
