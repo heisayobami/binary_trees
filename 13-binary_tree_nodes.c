00ecb7e2059247e9d7e59f0ecbfcac5e40ec1894
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-03-09 17:13:08 +0100

    Counter for node with child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,43 +7,10 @@
  * Return: number of nodes counted
  *         0 if tree is NULL
  */
-size_t binary_tree_nodes(const binary_tree_t *tree)
-{
-	if (!tree || (!tree->left && !tree->right))
-		return (0);
-
-	return (binary_tree_size(tree) - binary_tree_leaves(tree));
-}
-
-/**
- * binary_tree_size - measures the size of a binary tree
- * @tree: tree to measure the size of
- *
- * Return: size of the tree
- *         0 if tree is NULL
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
-}
-
-/**
- * binary_tree_leaves - counts the leaves in a binary tree
- * @tree: tree to count the leaves from
- *
- * Return: number of leaves
- *         0 if tree is NULL
- */
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-
-	if (!tree->left && !tree->right)
-		return (1);
-
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-}
+    size_t count = 0;
+    if (tree==NULL)
+        return 0;
+    count = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+    if (tree->left!=NULL || tree->right!=NULL)
+        return ++count;
+    return count;
