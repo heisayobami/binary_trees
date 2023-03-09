9448eea8abc019cddf1ecea18edf790cabf8f167
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-03-09 22:24:24 +0100

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -22,3 +22,23 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	
     return (pow(2, height)==leaf_nodes);
 }
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: tree to count the leaves from
+ *
+ * Return: number of leaves
+ *         0 if tree is NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    size_t count = 0;
+	
+    if (tree==NULL)
+        return 0;
+    
+    if (tree->left==NULL && tree->right==NULL)
+        return ++count;
+    
+    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
