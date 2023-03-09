6eba8ae19db409ac4675c8d0938f0e112e8166d6
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-03-09 17:55:28 +0100

    Balance factor
    Calculates the difference in height between the left sub-tree and the right sub-tree of a node.
    
    It's conventionally denoted as height of left minus height of right.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,15 +9,22 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int height_l, height_r;
+    int height_left, height_right;
+	
+    if (tree==NULL)
+        return 0;
+	
+    if (tree->left)
+        height_left = (int)binary_tree_height(tree->left);
+    else
+        height_left = -1;
 
-	if (!tree)
-		return (0);
-
-	height_l = tree->left ? (int)binary_tree_height(tree->left) : -1;
-	height_r = tree->right ? (int)binary_tree_height(tree->right) : -1;
-
-	return (height_l - height_r);
+    if (tree->right)
+        height_right = (int)binary_tree_height(tree->right);
+    else
+        height_right = -1;
+	
+    return (height_left-height_right);
 }
 
 /**
@@ -29,13 +36,18 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l = 0;
-	size_t height_r = 0;
-
-	if (!tree)
-		return (0);
-
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+    size_t height_left = 0, height_right = 0;
+    
+    if (tree==NULL)
+        return 0;
+    
+    if (tree->left)
+        height_left = 1 + binary_tree_height(tree->left);
+    if (tree->right)
+        height_right = 1 + binary_tree_height(tree->right);
+    
+    if (height_left > height_right)
+        return height_left;
+    else
+        return height_right;
 }
