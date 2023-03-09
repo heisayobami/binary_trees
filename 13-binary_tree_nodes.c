7d060c8ad44becce6d3b62b95f920c2dd900a97b
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-03-09 17:19:36 +0100

    Counter for nodes with child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,10 +10,13 @@
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
     size_t count = 0;
+    
     if (tree==NULL)
         return 0;
+    
     count = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
     if (tree->left!=NULL || tree->right!=NULL)
         return ++count;
+    
     return count;
 }
