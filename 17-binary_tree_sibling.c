fcaf8f2aad336f5e588aca189a724010ca75214c
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-03-09 22:58:54 +0100

    Siblings checker

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,10 +11,16 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node || !node->parent)
-		return (NULL);
-
-	if (node == node->parent->left)
-		return (node->parent->right);
-	return (node->parent->left);
+    if (!node || !node->parent)
+        return NULL;
+	
+    if (node->parent->left && node->parent->right)
+    {
+        if (node->parent->left == node)
+            return node->parent->right;
+        else
+            return node->parent->left;
+    }
+	
+    return NULL;
 }
