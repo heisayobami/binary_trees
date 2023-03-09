c7c60aced8d2f23902d94d116ac597f316f54718
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-03-09 01:04:37 +0100

    Height of a BT

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l = 0;
-	size_t height_r = 0;
-
-	if (!tree)
-		return (0);
-
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+    size_t height_left = 0, height_right = 0;
+	
+    if (tree==NULL)
+        return 0;
+	
+    if (tree->left)
+        height_left = 1 + binary_tree_height(tree->left);
+    if (tree->right)
+        height_right = 1 + binary_tree_height(tree->right);
+	
+    if (height_left > height_right)
+        return height_left;
+    else
+        return height_right;
 }
