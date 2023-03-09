bb32eeda50128c14731e975ee12ed2efce131175
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-03-09 17:21:07 +0100

    Leaf nodes counter

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,9 +10,12 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
     size_t count = 0;
+    
     if (tree==NULL)
         return 0;
+    
     if (tree->left==NULL && tree->right==NULL)
         return ++count;
+    
     return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
